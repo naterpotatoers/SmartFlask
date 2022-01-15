0fa3d860521904500ebf5ef529d99488bc78c0b6
Adrien Carrou <dridriswim@gmail.com>
2022-01-14 23:36:03 -0800

    Ultrasonic sensor class and implementation

---
+++ IoT/Heltec Transiever/lib/Ultrasonic_sensor.hpp
@@ -0,0 +1,71 @@
+#pragma once
+#include <Arduino.h>
+
+class UltrasonicSensor
+{
+
+private:
+#define SOUND_SPEED 0.034
+#define CM_TO_INCH 0.393701
+
+    int trigPin_;
+    int echoPin_;
+    long duration;
+    float distanceCm;
+    float distanceInch;
+    int print_count;
+
+public:
+    UltrasonicSensor(int trigPin, int echoPin)
+    {
+        trigPin_ = trigPin;
+        echoPin_ = echoPin;
+    }
+
+    int getTrigPin()
+    {
+        return trigPin_;
+    }
+
+    int getEchoPin()
+    {
+        return echoPin_;
+    }
+    // define sound speed in cm/uS
+
+    void SetUp()
+    {
+        pinMode(trigPin_, OUTPUT); // Sets the trigPin as an Output
+        pinMode(echoPin_, INPUT);  // Sets the echoPin as an Input
+    }
+
+    void SenseDistance()
+    {
+        // Clears the trigPin
+        digitalWrite(trigPin_, LOW);
+        delayMicroseconds(2);
+        // Sets the trigPin on HIGH state for 10 micro seconds
+        digitalWrite(trigPin_, HIGH);
+        delayMicroseconds(10);
+        digitalWrite(trigPin_, LOW);
+
+        // Reads the echoPin, returns the sound wave travel time in microseconds
+        duration = pulseIn(echoPin_, HIGH);
+        // Calculate the distance
+        distanceCm = duration * SOUND_SPEED / 2;
+        // Convert to inches
+        distanceInch = distanceCm * CM_TO_INCH;
+    }
+
+    void PrintData()
+    {
+        // Prints the distance in the Serial Monitor
+        Serial.println("Print Count: " + String(print_count));
+        Serial.print("Distance (cm): ");
+        Serial.println(distanceCm);
+        Serial.print("Distance (inch): ");
+        Serial.println(distanceInch);
+        print_count++;
+        delay(1000);
+    }
+};
--- IoT/Heltec Transiever/src/main.cpp
+++ IoT/Heltec Transiever/src/main.cpp
@@ -1,9 +1,27 @@
-#include <Arduino.h>
+// LoRa and Heltec
+#include <heltec.h>
+#include <LoRa.h>
+#include "E:\SmartFlask\IoT\Heltec Transiever\lib\Ultrasonic_sensor.hpp"
+
+
+//===To Implement After===
+// GPS Library
+//#include <TinyGPS++.h>
+// MPU Library
+//#include <Adafruit_MPU6050.h>
+//#include <Wire.h>
+
+//Intiatiated Sensor Objects
+UltrasonicSensor us_sensor(17, 2);
 
 void setup() {
+  Serial.begin(9600);
+  us_sensor.SetUp();
   // put your setup code here, to run once:
 }
 
 void loop() {
+  us_sensor.SenseDistance();
+  us_sensor.PrintData();
   // put your main code here, to run repeatedly:
 }
