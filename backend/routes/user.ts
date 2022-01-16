6b335b418b662cff1c19797dc0cefa1ef24cb737
Adrian Posadas <posadas.adrian20@gmail.com>
2022-01-15 17:45:20 -0800

    fixed get and post order

--- backend/routes/user.ts
+++ backend/routes/user.ts
@@ -17,10 +17,8 @@ var userInfo: User = {
   daily_target_water_level: 0,
 };
 
-user.post("/", (req, res) => {
-  const new_user = req.body;
-  res.jsonp(userInfo);
-  let query = `INSERT INTO user_table(email, password, flask_name, flask_size, user_weight, user_height, user_age, dietary_restrictions, daily_target_water_level) VALUES ('${new_user.email}', '${new_user.password}', '${new_user.flask_name}', ${new_user.flask_size}, ${new_user.user_weight}, ${new_user.user_height}, ${new_user.user_age}, '${new_user.dietary_restrictions}', ${new_user.daily_target_water_level})`;
+user.get("/", (req, res) => {
+  let query = "SELECT * FROM user_table";
   connection.query(query, function (err: Error, results) {
     if (err) throw err;
     res.json({
@@ -29,8 +27,10 @@ user.post("/", (req, res) => {
   });
 });
 
-user.get("/", (req, res) => {
-  let query = "SELECT * FROM user_table";
+user.post("/", (req, res) => {
+  const new_user = req.body;
+  res.jsonp(userInfo);
+  let query = `INSERT INTO user_table(email, password, flask_name, flask_size, user_weight, user_height, user_age, dietary_restrictions, daily_target_water_level) VALUES ('${new_user.email}', '${new_user.password}', '${new_user.flask_name}', ${new_user.flask_size}, ${new_user.user_weight}, ${new_user.user_height}, ${new_user.user_age}, '${new_user.dietary_restrictions}', ${new_user.daily_target_water_level})`;
   connection.query(query, function (err: Error, results) {
     if (err) throw err;
     res.json({
