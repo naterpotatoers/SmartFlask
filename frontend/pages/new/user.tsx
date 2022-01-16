1ef3808ac966b57146bb64fdc920d4e4cd97a2f9
naterpotatoers <nate.garze@hotmail.com>
2022-01-15 19:26:53 -0800

    adds new user page and user page

---
+++ frontend/pages/new/user.tsx
@@ -0,0 +1,6 @@
+function Add() {
+  return <div>Add User</div>;
+  // TODO: Should be a web form for adding a new user
+}
+
+export default Add;
--- frontend/pages/user/index.tsx
+++ frontend/pages/user/index.tsx
@@ -1,5 +1,6 @@
 function User() {
   return <div>User</div>;
+  // TODO: Should be a page showing all the user data
 }
 
 export default User;
