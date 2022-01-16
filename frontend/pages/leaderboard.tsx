f171f67959f8cfe6104418a070146e095ce1f1f7
naterpotatoers <nate.garze@hotmail.com>
2022-01-16 02:08:12 -0800

    updates

--- frontend/pages/leaderboard.tsx
+++ frontend/pages/leaderboard.tsx
@@ -1,6 +1,30 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+import styles from "../styles/Home.module.css";
+
 function Leaderboard() {
-  return <div>Leaderboard</div>;
-  // TODO: Should be a page showing leaderboard data
+  const [data, setData] = useState([]);
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch("http://localhost:5000/flask/leaderboard")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      });
+  }, []);
+
+  if (isLoading) return <p>Loading...</p>;
+
+  return (
+    <div>
+      <h1>{data[0].flask_name}</h1>
+      <p>{data[1].flask_name}</p>
+    </div>
+  );
 }
 
 export default Leaderboard;
